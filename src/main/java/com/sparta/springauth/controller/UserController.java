ba8437349c1cc9abada7db089916f9418f4bee2e
jwywoo <jwywoo26@gmail.com>
2023-09-02 18:42:23 +0900

    12th Commit: JWYWOO : Validation -> Exception handling UserController.java -> signup method

--- src/main/java/com/sparta/springauth/controller/UserController.java
+++ src/main/java/com/sparta/springauth/controller/UserController.java
@@ -4,19 +4,25 @@ import com.sparta.springauth.dto.LoginRequestDto;
 import com.sparta.springauth.dto.SignupRequestDto;
 import com.sparta.springauth.service.UserService;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
+@Slf4j
 @Controller
 @RequestMapping("/api")
 public class UserController {
 
     private final UserService userService;
 
-    public UserController(UserService userService){
+    public UserController(UserService userService) {
         this.userService = userService;
     }
 
@@ -24,17 +30,28 @@ public class UserController {
     public String loginPage() {
         return "login";
     }
-    // Signup
+
     @GetMapping("/user/signup")
     public String signupPage() {
         return "signup";
     }
 
     @PostMapping("/user/signup")
-    public String signup(@ModelAttribute SignupRequestDto requestDto) {
+    public String signup(@Valid SignupRequestDto requestDto, BindingResult bindingResult) {
+        // Validation 예외처리
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        if(fieldErrors.size() > 0) {
+            for (FieldError fieldError : bindingResult.getFieldErrors()) {
+                log.error(fieldError.getField() + " 필드 : " + fieldError.getDefaultMessage());
+            }
+            return "redirect:/api/user/signup";
+        }
+
         userService.signup(requestDto);
+
         return "redirect:/api/user/login-page";
     }
+}
 
 //    // Login
 //    @PostMapping("/user/login")
@@ -46,4 +63,3 @@ public class UserController {
 //        }
 //        return "redirect:/";
 //    }
-}
